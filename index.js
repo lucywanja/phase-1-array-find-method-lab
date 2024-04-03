6b0faca2dcb6850f0a608a670ce0ff922b52c508
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-04-03 22:59:14 +0300

    learnt how to use find method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(record) {
+    const winningRecord = record.find(entry => entry.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+    //(?  .) if winningRecord exists (i.e., not undefined), return winningRecord.year; otherwise, return undefined.
+  }
+  
